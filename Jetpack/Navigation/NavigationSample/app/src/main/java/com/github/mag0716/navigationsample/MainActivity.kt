18f95e180060b72bbecf0cd9b0c9b619fe403189
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-05-16 21:43:14 +0900

    タブ毎にスタックを管理し、タブ切り替えでもスタックを保持するように修正

--- Jetpack/Navigation/NavigationSample/app/src/main/java/com/github/mag0716/navigationsample/MainActivity.kt
+++ Jetpack/Navigation/NavigationSample/app/src/main/java/com/github/mag0716/navigationsample/MainActivity.kt
@@ -3,10 +3,19 @@ package com.github.mag0716.navigationsample
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+
+        const val HOME = "home"
+        const val DASHBOARD = "dashboard"
+        const val NOTIFICATIONS = "notifications"
+    }
 
     var homeFragment: ParentFragment? = null
     var dashboardFragment: ParentFragment? = null
@@ -38,7 +47,12 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             switchTab(R.id.navigation_home)
+        } else {
+            homeFragment = supportFragmentManager.findFragmentByTag(HOME) as? ParentFragment
+            dashboardFragment = supportFragmentManager.findFragmentByTag(DASHBOARD) as? ParentFragment
+            notificationsFragment = supportFragmentManager.findFragmentByTag(NOTIFICATIONS) as? ParentFragment
         }
+        Log.d(TAG, "onCreate : $homeFragment, $dashboardFragment, $notificationsFragment")
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,27 +69,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun switchTab(id: Int) {
+        Log.d(TAG, "switchTab : $id")
         var title: String? = null
         var fragment: ParentFragment? = null
+        var newFragment = true
         when (id) {
             R.id.navigation_home -> {
-                title = "home"
+                title = HOME
                 if (homeFragment == null) {
                     homeFragment = ParentFragment.newInstance(title)
+                } else {
+                    newFragment = false
                 }
                 fragment = homeFragment
             }
             R.id.navigation_dashboard -> {
-                title = "dashboard"
+                title = DASHBOARD
                 if (dashboardFragment == null) {
                     dashboardFragment = ParentFragment.newInstance(title)
+                } else {
+                    newFragment = false
                 }
                 fragment = dashboardFragment
             }
             R.id.navigation_notifications -> {
-                title = "notifications"
+                title = NOTIFICATIONS
                 if (notificationsFragment == null) {
                     notificationsFragment = ParentFragment.newInstance(title)
+                } else {
+                    newFragment = false
                 }
                 fragment = notificationsFragment
             }
@@ -84,7 +106,15 @@ class MainActivity : AppCompatActivity() {
         if (fragment != null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.setPrimaryNavigationFragment(fragment)
-            transaction.replace(R.id.container, fragment)
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
+            if (currentFragment != null) {
+                transaction.detach(currentFragment)
+            }
+            if (newFragment) {
+                transaction.add(R.id.container, fragment, title)
+            } else {
+                transaction.attach(fragment)
+            }
             transaction.commit()
         }
 
