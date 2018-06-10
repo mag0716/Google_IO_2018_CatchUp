af32bb9db47dd029aa59154441ec172d58cec200
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-06-10 22:30:06 +0900

    1.0.0-alpha02 で解消された不具合を確認

--- Jetpack/Navigation/NavigationSample/nestednavigation/src/main/java/com/github/mag0716/nestednavigation/ProfileFragment.kt
+++ Jetpack/Navigation/NavigationSample/nestednavigation/src/main/java/com/github/mag0716/nestednavigation/ProfileFragment.kt
@@ -6,25 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        button.setOnClickListener { view.findNavController().navigate(R.id.action_login) }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // TODO: 未ログインの場合に LoginFragment へ遷移させたい
-        // java.lang.IllegalArgumentException: navigation destination com.github.mag0716.nestednavigation:id/action_login is unknown to this NavController
-        //findNavController().navigate(R.id.action_login)
     }
 }
