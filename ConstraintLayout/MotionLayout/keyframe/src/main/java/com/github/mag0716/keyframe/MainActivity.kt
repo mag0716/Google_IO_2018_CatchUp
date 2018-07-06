8029e009ffa557bd0dbb05c142b570419d87a877
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-07-06 23:36:31 +0900

    pathRelative, deltaRelative のサンプルを追加

--- ConstraintLayout/MotionLayout/keyframe/src/main/java/com/github/mag0716/keyframe/MainActivity.kt
+++ ConstraintLayout/MotionLayout/keyframe/src/main/java/com/github/mag0716/keyframe/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.support.constraint.motion.MotionLayout
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener {
+class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener, AdapterView.OnItemSelectedListener {
 
     companion object {
         const val TAG = "keyframe"
@@ -19,6 +21,14 @@ class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener {
         setContentView(R.layout.activity_main)
 
         motion_layout.setTransitionListener(this)
+        button.setOnClickListener {
+            if (currentId == R.id.start) {
+                motion_layout.transitionToEnd()
+            } else {
+                motion_layout.transitionToStart()
+            }
+        }
+        spinner.onItemSelectedListener = this
     }
 
     override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
@@ -28,5 +38,14 @@ class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener {
 
     override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
         Log.d(TAG, "onTransitionChange : startId = $startId, endId = $endId")
+    }
+
+    override fun onNothingSelected(adapter: AdapterView<*>?) {
+    }
+
+    override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val motionName = resources.getStringArray(R.array.items)[position]
+        val motionId = resources.getIdentifier(motionName, "xml", packageName)
+        motion_layout.loadLayoutDescription(motionId)
     }
 }
