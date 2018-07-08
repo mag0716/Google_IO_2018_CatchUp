ca98b8c1f79616745a4d9ea341211cba6cf9bad5
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-07-08 22:49:20 +0900

    MotionHelper のサンプルモジュール追加

---
+++ ConstraintLayout/MotionLayout/motionhelper/src/main/java/com/github/mag0716/motionhelper/Fade.kt
@@ -0,0 +1,17 @@
+package com.github.mag0716.motionhelper
+
+import android.content.Context
+import android.support.constraint.motion.MotionHelper
+import android.util.AttributeSet
+import android.view.View
+
+class Fade : MotionHelper {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun setProgress(view: View, progress: Float) {
+        view.alpha = progress
+    }
+}
