a7d6912d0b055c4381b77bc8c83f50cdb68484dd
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-07-18 22:10:48 +0900

    Toolbar 単体で利用するサンプル追加

--- Jetpack/Navigation/NavigationSample/toolbar/src/main/java/com/github/mag0716/toolbar/MainActivity.kt
+++ Jetpack/Navigation/NavigationSample/toolbar/src/main/java/com/github/mag0716/toolbar/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.mag0716.toolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ Jetpack/Navigation/NavigationSample/toolbar/src/main/java/com/github/mag0716/toolbar/SecondFragment.kt
@@ -0,0 +1,17 @@
+package com.github.mag0716.toolbar
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+}
