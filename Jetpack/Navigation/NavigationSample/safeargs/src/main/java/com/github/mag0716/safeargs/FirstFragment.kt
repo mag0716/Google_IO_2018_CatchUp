b3a5cf6622a600dc188955c80548ee24543a5959
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-07-18 22:36:32 +0900

    Safe Args を試すモジュールを追加

---
+++ Jetpack/Navigation/NavigationSample/safeargs/src/main/java/com/github/mag0716/safeargs/FirstFragment.kt
@@ -0,0 +1,24 @@
+package com.github.mag0716.safeargs
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener { view.findNavController().navigate(R.id.action_move_to_second) }
+    }
+}
