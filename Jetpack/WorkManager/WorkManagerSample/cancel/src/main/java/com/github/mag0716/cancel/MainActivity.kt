39f221e7a11e025a4f26fb14b2819e5464c30c71
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-07-26 23:27:05 +0900

    タスクのキャンセルを試すモジュール追加

---
+++ Jetpack/WorkManager/WorkManagerSample/cancel/src/main/java/com/github/mag0716/cancel/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.github.mag0716.cancel
+
+import android.arch.lifecycle.Observer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.github.mag0716.common.LoggingWorker
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "Cancel"
+    }
+
+    private val work = OneTimeWorkRequestBuilder<LoggingWorker>()
+            .setInputData(LoggingWorker.createInputData(TAG))
+            .build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        enqueue_button.setOnClickListener { enqueue() }
+        cancel_button.setOnClickListener { cancel() }
+    }
+
+
+    private fun enqueue() {
+        val work = OneTimeWorkRequestBuilder<LoggingWorker>()
+                .setInputData(LoggingWorker.createInputData(TAG))
+                .build()
+        // ENQUEUED -> RUNNING -> SUCCEEDED の順だが、ENQUEUED が出力されない場合もある
+        WorkManager.getInstance().getStatusById(work.id)
+                .observe(this, Observer { status ->
+                    Log.d(TAG, "observe : status = $status")
+                })
+        WorkManager.getInstance().enqueue(work)
+    }
+
+    private fun cancel() {
+        WorkManager.getInstance().cancelWorkById(work.id)
+    }
+}
