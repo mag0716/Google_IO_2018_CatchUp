9c83ac60ca9617b86618db12be14cbbc2d214aa4
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-08-22 23:39:52 +0900

    View に対して verticalBias を変更する処理を実行

--- ConstraintLayout/ConstraintLayout/properties/src/main/java/com/github/mag0716/properties/MainActivity.kt
+++ ConstraintLayout/ConstraintLayout/properties/src/main/java/com/github/mag0716/properties/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.github.mag0716.properties
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintProperties
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var top = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // これは適用される
+        applyTextProperties()
+
+        // こっちは適用されない
+        button.setOnClickListener {
+            applyTextProperties()
+        }
+    }
+
+    private fun applyTextProperties() {
+        // ConstraintProperties のコンストラクタに ConstraintLayout の子供の View を渡す
+        ConstraintProperties(text).apply {
+            val bias = if (top) 1.0f else 0.0f
+            top = !top
+            verticalBias(bias).apply()
+        }
     }
 }
