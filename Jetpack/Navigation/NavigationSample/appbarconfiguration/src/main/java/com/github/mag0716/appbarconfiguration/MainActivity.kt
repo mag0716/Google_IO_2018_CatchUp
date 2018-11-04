9a3341086e86ca55c45ef012428a9b3ca8d83f67
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-11-04 12:15:28 +0900

    AppBarConfiguration をためすモジュール追加

---
+++ Jetpack/Navigation/NavigationSample/appbarconfiguration/src/main/java/com/github/mag0716/appbarconfiguration/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.github.mag0716.appbarconfiguration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(toolbar, findNavController(R.id.container))
+    }
+}
