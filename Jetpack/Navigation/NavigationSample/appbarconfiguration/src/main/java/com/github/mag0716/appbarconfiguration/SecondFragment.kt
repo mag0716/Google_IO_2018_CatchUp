7e23f4b34d0375368bc907163d3cb44fa02b1d8b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-11-04 12:45:09 +0900

    AppBarConfiguration と Toolbar を紐づける

--- Jetpack/Navigation/NavigationSample/appbarconfiguration/src/main/java/com/github/mag0716/appbarconfiguration/SecondFragment.kt
+++ Jetpack/Navigation/NavigationSample/appbarconfiguration/src/main/java/com/github/mag0716/appbarconfiguration/SecondFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class SecondFragment : androidx.fragment.app.Fragment() {
 
@@ -12,5 +14,10 @@ class SecondFragment : androidx.fragment.app.Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener { view.findNavController().navigate(R.id.action_move_to_third) }
     }
 }
