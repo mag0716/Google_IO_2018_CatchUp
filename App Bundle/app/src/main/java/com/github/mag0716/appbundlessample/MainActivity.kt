e1e123795f515f3346deadb885c73f125226b7fe
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-02-05 23:18:04 +0900

    feature モジュールに Activity を持たせる

--- App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
+++ App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.github.mag0716.appbundlessample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            launchFeatureModule()
+        }
+    }
+
+    private fun launchFeatureModule() {
+        val intent = Intent(Intent.ACTION_VIEW).setClassName(
+                "com.github.mag0716.dynamic_feature",
+                "com.github.mag0716.dynamic_feature.FeatureActivity"
+        )
+        startActivity(intent)
     }
 }
---
+++ App Bundle/dynamic_feature/src/main/java/com/github/mag0716/dynamic_feature/FeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.github.mag0716.dynamic_feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}
