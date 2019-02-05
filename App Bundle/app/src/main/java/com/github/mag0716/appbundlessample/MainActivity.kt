641992b2795a5e24a837d6ae1aa2ffc9af841dcc
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-02-05 22:49:38 +0900

    AndroidX に migrate

--- App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
+++ App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.github.mag0716.appbundlessample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
