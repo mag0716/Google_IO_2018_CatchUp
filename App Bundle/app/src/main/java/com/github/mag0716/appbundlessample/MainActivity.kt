3d2a202b7609749776ecc144aa548ada38786c32
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-02-06 22:29:48 +0900

    com.google.android.play:core を利用し、モジュールが存在しているかをチェック

--- App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
+++ App Bundle/app/src/main/java/com/github/mag0716/appbundlessample/MainActivity.kt
@@ -2,26 +2,67 @@ package com.github.mag0716.appbundlessample
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.splitinstall.*
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SplitInstallStateUpdatedListener {
 
-    lateinit var button: Button
+    companion object {
+        const val TAG = "DynamicFeature"
+    }
+
+    private lateinit var button: Button
+
+    private lateinit var manager: SplitInstallManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        manager = SplitInstallManagerFactory.create(this)
+
         button = findViewById(R.id.button)
         button.setOnClickListener {
+            loadModuleIfNeeded()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        manager.registerListener(this)
+    }
+
+    override fun onPause() {
+        manager.unregisterListener(this)
+        super.onPause()
+    }
+
+    override fun onStateUpdate(state: SplitInstallSessionState?) {
+        Log.d(TAG, "onStateUpdate : $state")
+        if (state?.status() == SplitInstallSessionStatus.INSTALLED) {
             launchFeatureModule()
+        }
+    }
+
+    private fun loadModuleIfNeeded() {
+        Log.d(TAG, "loadModuleIfNeeded : ${manager.installedModules}")
+        val moduleName = "dynamic_feature"
+        if (manager.installedModules.contains(moduleName)) {
+            launchFeatureModule()
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                    .addModule(moduleName)
+                    .build()
+            manager.startInstall(request)
         }
     }
 
     private fun launchFeatureModule() {
         val intent = Intent(Intent.ACTION_VIEW).setClassName(
-                "com.github.mag0716.dynamic_feature",
+                packageName,
                 "com.github.mag0716.dynamic_feature.FeatureActivity"
         )
         startActivity(intent)
--- App Bundle/dynamic_feature/src/main/java/com/github/mag0716/dynamic_feature/FeatureActivity.kt
+++ App Bundle/dynamic_feature/src/main/java/com/github/mag0716/dynamic_feature/FeatureActivity.kt
@@ -1,9 +1,16 @@
 package com.github.mag0716.dynamic_feature
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.splitcompat.SplitCompat
 
 class FeatureActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        SplitCompat.install(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
