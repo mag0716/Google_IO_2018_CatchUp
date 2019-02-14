80b67cf2ee99948beaa16722c056b94faea76f1a
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-02-14 22:40:07 +0900

    正常に動作する valid_dynamic_feature を追加

---
+++ App Bundle/valid_dynamic_feature/src/main/java/com/github/mag0716/valid_dynamic_feature/FeatureActivity.kt
@@ -0,0 +1,26 @@
+package com.github.mag0716.valid_dynamic_feature
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mag0716.valid_module.TextProvider
+import com.google.android.play.core.splitcompat.SplitCompat
+
+class FeatureActivity : AppCompatActivity() {
+
+    private val textProvider = TextProvider()
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        SplitCompat.install(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+
+        val textView = findViewById<TextView>(R.id.text)
+        textView.text = textProvider.provide()
+    }
+}
---
+++ App Bundle/valid_module/src/main/java/com/github/mag0716/valid_module/TextProvider.kt
@@ -0,0 +1,5 @@
+package com.github.mag0716.valid_module
+
+class TextProvider {
+    fun provide() = "valid_dynamic_feature"
+}
