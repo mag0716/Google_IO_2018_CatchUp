242c8e62f1bed4ae1f30763aebe9d9c717a27e9e
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-06-21 23:05:37 +0900

    attribute リネーム、TransitionListener の実装を追加

--- ConstraintLayout/MotionLayout/motionhelper/src/main/java/com/github/mag0716/motionhelper/MainActivity.kt
+++ ConstraintLayout/MotionLayout/motionhelper/src/main/java/com/github/mag0716/motionhelper/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.github.mag0716.motionhelper
 
 import android.os.Bundle
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener {
@@ -25,5 +25,13 @@ class MainActivity : AppCompatActivity(), MotionLayout.TransitionListener {
 
     override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
         Log.d(TAG, "onTransitionChange : startId = $startId, endId = $endId")
+    }
+
+    override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
+        Log.d(TAG, "onTransitionTrigger : triggerId = $triggerId, positive = $positive, progress = $progress")
+    }
+
+    override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+        Log.d(TAG, "onTransitionStarted : startId = $startId, endId = $endId")
     }
 }
